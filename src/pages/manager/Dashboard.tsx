a90dedf2afdfd79ee349f6677d50f6c1e8c8795a
BLARD <peteromu76@email.com>
2024-11-09 00:50:54 +0100

    new pull

--- src/pages/manager/Dashboard.tsx
+++ src/pages/manager/Dashboard.tsx
@@ -1,3 +1,25 @@
+// import { FC } from "react";
+// import StatsDashboard from "../../features/manager/DashboardStats";
+// import ApexLineChart from "../../features/super-user/dashboard/ApexLineChart";
+// import DashboardTable from "../../features/super-user/dashboard/DashboardTable";
+
+// const ManagerDashboard: FC = () => {
+//   return (
+//     <div className="p-8 md:p-16 flex flex-col gap-8 bg-slate-100 hide-scrollbar">
+//       <h1 className="font-bold text-4xl">Dashboard</h1>
+//       <StatsDashboard/>
+//       <ApexLineChart />
+//       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+//         <div className=""></div>
+//         <div className=""></div>
+//       </div>
+      
+//     </div>
+//   );
+// };
+
+// export default ManagerDashboard;
+
 import { FC } from "react";
 import StatsDashboard from "../../features/manager/DashboardStats";
 import ApexLineChart from "../../features/super-user/dashboard/ApexLineChart";
@@ -7,9 +29,12 @@ const ManagerDashboard: FC = () => {
   return (
     <div className="p-8 md:p-16 flex flex-col gap-8 bg-slate-100 hide-scrollbar">
       <h1 className="font-bold text-4xl">Dashboard</h1>
-      <StatsDashboard/>
+      <StatsDashboard />
       <ApexLineChart />
-      <DashboardTable />
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <div className="bg-white p-4 rounded h-auto min-h-[600px] shadow"> </div>
+        <div className="bg-white p-4 rounded h-auto min-h-[600px] shadow"></div>
+      </div>
     </div>
   );
 };
