d3651b96f4dfec2cfd79fe12685e6a16ae719325
BLARD <peteromu76@email.com>
2025-01-09 15:27:38 +0100

    auth modified

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
-import React, {  FC, ReactNode, Suspense } from "react";
+import React, { FC, ReactNode } from "react";
 import { useAppContext } from "./context/AppContext";
 import { useAxiosInterceptor } from "./hooks/useAxiosInterceptor";
-import Spinner from "./ui/utils/Spinner";
-
 // Dynamic imports
 const Dashboard = React.lazy(() => import("./pages/Dashboard"));
 const LoginPage = React.lazy(() => import("./pages/auth/LoginPage"));
@@ -49,7 +47,6 @@ const Index = React.lazy(() => import("./pages/Index"));
 const AuditorLayout = React.lazy(() => import("./ui/layouts/AuditorLayout"));
 const Settings = React.lazy(() => import("./pages/settings/Settings"));
 const AdminRule = React.lazy(() => import("./pages/AdminRule"));
-
 
 // ProtectedRoute Component
 interface ProtectedProps {
@@ -102,8 +99,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
 
-<Suspense fallback={<Spinner/>}>
-<Routes>
+      <Routes>
         {/* Public Routes */}
         <Route path="/" element={<Index />} />
         <Route path="/login" element={<LoginPage />} />
@@ -233,8 +229,6 @@ function App() {
         {/* Fallback Route */}
         <Route path="*" element={<Page404 />} />
       </Routes>
-</Suspense>
-      
 
       <Toaster
         position="top-center"
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,23 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { AppProvider } from "./context/AppContext.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { RuleProvider } from "./features/manager/rules/RuleContext.tsx";
+import CustomLoader from "./ui/utils/CustomLoader.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppProvider>
-        <RuleProvider>
-          <App />
-        </RuleProvider>
-      </AppProvider>
-    </BrowserRouter>
+    <Suspense fallback={<CustomLoader />}>
+      <BrowserRouter>
+        <AppProvider>
+          <RuleProvider>
+            <App />
+            <App />
+          </RuleProvider>
+        </AppProvider>
+      </BrowserRouter>
+    </Suspense>
   </StrictMode>
 );
