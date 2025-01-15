26479bcd73865c1c08c78a363e94a1a5bb4eb611
BLARD <peteromu76@email.com>
2025-01-15 15:08:31 +0100

    Error boundary added

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,27 @@
+// import { StrictMode, Suspense } from "react";
+// import { createRoot } from "react-dom/client";
+// import App from "./App.tsx";
+// import "./index.css";
+// import { AppProvider } from "./context/AppContext.tsx";
+// import { BrowserRouter } from "react-router-dom";
+// import { RuleProvider } from "./features/manager/rules/RuleContext.tsx";
+// import CustomLoader from "./ui/utils/CustomLoader.tsx";
+
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <Suspense fallback={<CustomLoader />}>
+//       <BrowserRouter>
+//         <AppProvider>
+//           <RuleProvider>
+//             <App />
+//           </RuleProvider>
+//         </AppProvider>
+//       </BrowserRouter>
+//     </Suspense>
+//   </StrictMode>
+// );
+
+
 import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -6,17 +30,20 @@ import { AppProvider } from "./context/AppContext.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { RuleProvider } from "./features/manager/rules/RuleContext.tsx";
 import CustomLoader from "./ui/utils/CustomLoader.tsx";
+import ErrorBoundary from "./ui/utils/ErrorBoundary.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Suspense fallback={<CustomLoader />}>
-      <BrowserRouter>
-        <AppProvider>
-          <RuleProvider>
-            <App />
-          </RuleProvider>
-        </AppProvider>
-      </BrowserRouter>
-    </Suspense>
+    <ErrorBoundary>
+      <Suspense fallback={<CustomLoader />}>
+        <BrowserRouter>
+          <AppProvider>
+            <RuleProvider>
+              <App />
+            </RuleProvider>
+          </AppProvider>
+        </BrowserRouter>
+      </Suspense>
+    </ErrorBoundary>
   </StrictMode>
 );
